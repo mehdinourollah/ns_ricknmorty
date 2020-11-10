80acd149993230c5db6f112cd771894e0ec70fef
meiki <mehdinourollah@gmail.com>
2020-11-10 17:50:01 +0300

    some dev configs

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -3,6 +3,7 @@ import { NativeScriptConfig } from '@nativescript/core';
 export default {
   id: 'com.mehdi.ricknmorty',
   appResourcesPath: 'App_Resources',
+  webpackConfigPath: 'webpack.config.js',
   android: {
     v8Flags: '--expose_gc',
     markingMode: 'none'
